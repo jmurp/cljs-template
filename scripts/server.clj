e128b0b9985e06e0bfc88b1872376fa9ed07b9bf
Jake Murphy <jmurphy@protonmail.com>
2018-11-11 15:02:54 -0500

    cljs template

---
+++ scripts/server.clj
@@ -0,0 +1,21 @@
+(require '[ring.adapter.jetty :refer [run-jetty]])
+(require '[ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+(require '[ring.util.response :refer [resource-response content-type]])
+
+(defn handler [req]
+  (or
+    (when (= "/" (:uri req))
+     (some->
+       (resource-response "index.html" {:root "public"})
+       (content-type "text/html; charset=utf-8")))
+    (when (= "/test" (:uri req))
+      (some->
+        (resource-response "test.html" {:root "public"})
+        (content-type "text/html; charset=utf-8")))
+    {:status 404
+     :headers {"Content-type" "text/html"}
+     :body "Not found"}))
+
+(run-jetty
+  (wrap-defaults handler site-defaults)
+  {:port 4000 :join? false})
---
+++ src/hello_world/core.cljs
@@ -0,0 +1,3 @@
+(ns hello-world.core
+  (:require [react]
+           [react-dom]))
